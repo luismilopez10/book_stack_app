59f446c58d9f8f73d9c953f911eadd7a1e592e4a
Luis Miguel Lopez Lopez <luimilop+cib@bancolombia.com.co>
2025-10-16 19:54:25 -0500

    refactor(theme_notifier): Se refactoriza el theme notifier

--- lib/theme/theme_notifier.dart
+++ lib/theme/theme_notifier.dart
@@ -1,16 +1,15 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeNotifier with ChangeNotifier {
   static const String selectTheme = 'ThemeSharePreferences';
-  ThemeMode? _themeMode;
 
-  ThemeMode? getThemeMode() => _themeMode;
+  ThemeMode? _themeMode;
+  ThemeMode? get themeMode => _themeMode;
 
   Future<void> setThemeMode(ThemeMode theme) async {
     final SharedPreferences instance = await SharedPreferences.getInstance();
-    await instance.setString(selectTheme, describeEnum(theme));
+    await instance.setString(selectTheme, theme.name);
     _themeMode = theme;
     notifyListeners();
   }
@@ -22,20 +21,13 @@ class ThemeNotifier with ChangeNotifier {
   }
 
   ThemeMode _refreshSystemTheme(String? theme) {
-    ThemeMode themeMode;
-    if (theme == '' || theme == null) {
-      themeMode = ThemeMode.system;
-    } else {
-      themeMode = getEnum(ThemeMode.values, theme);
+    if (theme == null || theme.isEmpty) {
+      return ThemeMode.system;
     }
-    return themeMode;
+
+    return ThemeMode.values.firstWhere(
+      (ThemeMode mode) => mode.name == theme,
+      orElse: () => ThemeMode.system,
+    );
   }
-
-  static bool _getEnum(type, String style) => type
-      .toString()
-      .replaceAll(RegExp(r'^[a-z]+\.', caseSensitive: false), '')
-      .contains(style);
-
-  static T getEnum<T>(List<T> values, String style) =>
-      values.firstWhere((e) => _getEnum(e, style));
 }
